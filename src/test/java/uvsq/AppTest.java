f73dd498f4feeebfaf6fdd0467b7d3c5a40eb6f8
plicence <p.licencedev@gmail.com>
2020-03-29 17:57:03 +0200

    Ajout complémentaires

--- src/test/java/uvsq/AppTest.java
+++ src/test/java/uvsq/AppTest.java
@@ -70,9 +70,67 @@ public class AppTest
     	
     	
     }
-    
-    
-    
-    
+
+
+    @Test
+	public void testAddition(){
+		MoteurRpn moteurRpn = new MoteurRpn();
+		moteurRpn.ajoutValeur(10);
+		moteurRpn.ajoutValeur(5);
+		moteurRpn.application(Operation.PLUS);
+		Stack<Integer> test = new Stack<>();
+		test.addAll(moteurRpn.retourner());
+		assertEquals(15, test.pop(), 0);
+	}
+
+	@Test
+	public void testSoustraction(){
+		MoteurRpn moteurRpn = new MoteurRpn();
+		moteurRpn.ajoutValeur(10);
+		moteurRpn.ajoutValeur(5);
+		moteurRpn.application(Operation.MOINS);
+		Stack<Integer> test = new Stack<>();
+		test.addAll(moteurRpn.retourner());
+		assertEquals(5, test.pop(), 0);
+	}
+
+	@Test
+	public void testMultiplication(){
+		MoteurRpn moteurRpn = new MoteurRpn();
+		moteurRpn.ajoutValeur(10);
+		moteurRpn.ajoutValeur(5);
+		moteurRpn.application(Operation.MULT);
+		Stack<Integer> test = new Stack<>();
+		test.addAll(moteurRpn.retourner());
+		assertEquals(50, test.pop(), 0);
+	}
+
+	@Test
+	public void testDivision(){
+		MoteurRpn moteurRpn = new MoteurRpn();
+		moteurRpn.ajoutValeur(10);
+		moteurRpn.ajoutValeur(5);
+		moteurRpn.application(Operation.DIV);
+		Stack<Integer> test = new Stack<>();
+		test.addAll(moteurRpn.retourner());
+		assertEquals(2, test.pop(), 0);
+	}
+
+	@Test
+	public void testEnregistrerMoteur(){
+		MoteurRpn moteurRpn = new MoteurRpn();
+		moteurRpn.ajoutValeur(10);
+		moteurRpn.ajoutValeur(5);
+		Stack<Integer> test = new Stack<>();
+		test.addAll(moteurRpn.retourner());
+		Stack<Integer> compare = new Stack<>();
+		compare.push(10);
+		compare.push(5);
+		for(Integer i : test){
+			assertEquals(compare.pop(), test.pop(), 0);
+		}
+
+	}
+
 
 }
